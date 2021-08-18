df759f88d71547de9a886ff4253ffe2413d8da05
jerryr <jerry@163.com>
2021-08-18 11:43:13 +0800

    818

--- exercise/src/com/jerryr/offer/JZ15/ReverseList.java
+++ exercise/src/com/jerryr/offer/JZ15/ReverseList.java
@@ -6,20 +6,59 @@ import com.jerryr.utils.ListNode;
 import java.util.Stack;
 
 public class ReverseList {
-    public ListNode ReverseList(ListNode head) {
-        Stack stack = new Stack();
-        ListNode temp = head;
-        while (temp != null){
-            stack.push(temp);
-            temp = temp.next;
+    /**
+     * @description:
+     * 输入一个链表，反转链表后，输出新链表的表头。
+     * 利用栈后进先出的特点进行链表反转
+     * @param head
+     * @return com.jerryr.utils.ListNode
+     * @author: jerryr
+     * @dateTime: 2021/8/18 11:02
+     */
+    public static ListNode ReverseList(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+        Stack<ListNode> stack = new Stack<>();
+        ListNode index = head;
+        while (index != null){
+            stack.push(index);
+            index = index.next;
         }
-        ListNode pre =null ;
-        ListNode suf =null;
+        ListNode current = stack.pop();
+        ListNode  headIndex = current;
         while (!stack.isEmpty()){
-
-            pre = (ListNode) stack.pop();
-            suf = pre.next;
+            ListNode temp = stack.pop();
+            current.next = temp;
+            current = current.next;
         }
-        return pre;
+        current.next = null;
+        return headIndex;
+    }
+    /**
+     * @description: 遍历链表的时候，将每一个节点反转，作为新链表的头节点
+     * @param head
+     * @return com.jerryr.utils.ListNode
+     * @author: jerryr
+     * @dateTime: 2021/8/18 11:06
+     */
+    public static ListNode ReverseList1(ListNode head) {
+       ListNode newHead = null;
+       while (head != null){
+            ListNode temp = head.next;
+            head.next = newHead;
+            newHead = head;
+            head = temp;
+       }
+       return newHead;
+    }
+
+    public static void main(String[] args) {
+        ListNode listNode = new ListNode(1);
+        listNode.add(2);
+        listNode.add(3);
+        listNode.print();
+        ListNode listNode1 = ReverseList(listNode);
+        System.out.println();
+        listNode1.print();
     }
 }
